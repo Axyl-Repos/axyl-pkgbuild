92c07a3a275892c83e4043862eee2ae2a13c596f
Kungger-git <matthewriel63@gmail.com>
2021-10-07 16:24:20 +0800

    fixed bug when building calamares deleting a dependency

--- local_pkgbuild/axyl-calamares/build.sh
+++ local_pkgbuild/axyl-calamares/build.sh
@@ -10,7 +10,7 @@ echo "################################################################"
 echo "################### Moved the .zst file to x86_64 folder"
 echo "################################################################"
 find . -type d -not -path "." -exec sudo rm -r "{}" \;
-find . -type f -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name -not -name  "49-nopasswd-calamares.rules" -not -name "calamares_polkit" -not -name "calamares.desktop" -not -name "calamares-3.2.44.3" -not -name "*.pkg.tar.zst" -delete
+find . -type f -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name  "49-nopasswd-calamares.rules" -not -name "calamares_polkit" -not -name "calamares.desktop" -not -name "calamares-3.2.44.3" -not -name "*.pkg.tar.zst" -delete
 echo "################################################################"
 echo "################### Removed all redundant files and folders"
 echo "################################################################"
