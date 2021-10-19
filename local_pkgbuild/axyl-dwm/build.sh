64b3ed67bc8e3081b89695fc76e3094c7618e970
Kungger-git <matthewriel63@gmail.com>
2021-10-19 08:45:05 +0800

    removed post install

--- local_pkgbuild/axyl-dwm/build.sh
+++ local_pkgbuild/axyl-dwm/build.sh
@@ -10,7 +10,7 @@ echo "################################################################"
 echo "################### Moved the .zst file to x86_64 folder"
 echo "################################################################"
 find . -type d -not -path "." -exec sudo rm -r "{}" \;
-find . -type f -not -name "axyl-dwm.install" -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name "*.pkg.tar.zst" -delete
+find . -type f -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name "*.pkg.tar.zst" -delete
 echo "################################################################"
 echo "################### Removed all redundant files and folders"
 echo "################################################################"
