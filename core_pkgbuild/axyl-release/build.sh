bf36412af804373ae7c93303bec793a45b518df4
Kungger-git <matthewriel63@gmail.com>
2021-10-28 09:46:21 +0800

    lsb & os release hooks fix

--- core_pkgbuild/axyl-release/build.sh
+++ core_pkgbuild/axyl-release/build.sh
@@ -9,8 +9,8 @@ mv *.pkg.tar.zst ../../x86_64
 echo "################################################################"
 echo "################### Moved the .zst file to x86_64 folder"
 echo "################################################################"
-find . -type d -not -path "." -exec sudo rm -r "{}" \;
-find . -type f -not -name "build.sh" -not -name "lsb-release" -not -name "*.install" -not -name PKGBUILD -not -name "*.pkg.tar.zst" -delete
+find . -type d -not -path "." -not -path "./files" -exec sudo rm -r "{}" \;
+find . -type f -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name "*.pkg.tar.zst" -delete
 echo "################################################################"
 echo "################### Removed all redundant files and folders"
 echo "################################################################"
