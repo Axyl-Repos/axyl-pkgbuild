c0f65e35cde9c173bf3ca8ee10fccbc5e0c7be89
Kungger-git <matthewriel63@gmail.com>
2021-11-01 13:44:59 +0800

    added spectrwm-git to aurpackages list

---
+++ 3rdparty_pkgbuild/3rdparty/shell-color-scripts/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "################################################################"
+echo "################### Making the package"
+echo "################################################################"
+makepkg -sc --noconfirm --skippgpcheck
+mkdir -p ../../../x86_64
+mv *.pkg.tar.zst ../../../x86_64
+echo "################################################################"
+echo "################### Moved the .zst file to x86_64 folder"
+echo "################################################################"
+find . -type d -not -path "." -exec sudo rm -r "{}" \;
+find . -type f -not -name "*.sh" -not -name "*.install" -not -name PKGBUILD -not -name "*.pkg.tar.zst" -delete
+echo "################################################################"
+echo "################### Removed all redundant files and folders"
+echo "################################################################"
