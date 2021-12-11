16c3e0677675ca4c8f359ea520234bbe0fe09cd1
Kungger-git <matthewriel63@gmail.com>
2021-12-11 14:10:57 +0800

    Remove build script from root dir of repo

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -rf ./3rdparty_pkgbuild/3rdparty
-cd ./3rdparty_pkgbuild
-./get-pkgs.sh
-cd 3rdparty
-for d in ./*/ ; do (cd "$d" && ./build.sh); done
-cd ../../local_pkgbuild
-for d in ./*/ ; do (cd "$d" && ./build.sh); done
-yes | yay -c
