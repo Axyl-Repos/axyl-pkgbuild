1658d4520bd79c9b42072ca988c4dc5a10d87033
Kungger-git <matthewriel63@gmail.com>
2021-12-15 12:20:06 +0800

    Update calamares

--- core_pkgbuild/axyl-calamares/build.sh
+++ core_pkgbuild/axyl-calamares/build.sh
@@ -10,7 +10,7 @@ echo "################################################################"
 echo "################### Moved the .zst file to x86_64 folder"
 echo "################################################################"
 find . -type d -not -path "." -exec sudo rm -r "{}" \;
-find . -type f -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name  "49-nopasswd-calamares.rules" -not -name "calamares_polkit" -not -name "calamares.desktop" -not -name "calamares-3.2.48" -not -name "*.pkg.tar.zst" -delete
+find . -type f -not -name "build.sh" -not -name "*.install" -not -name PKGBUILD -not -name  "49-nopasswd-calamares.rules" -not -name "calamares_polkit" -not -name "calamares.desktop" -not -name "calamares-3.2.49.1" -not -name "*.pkg.tar.zst" -delete
 echo "################################################################"
 echo "################### Removed all redundant files and folders"
 echo "################################################################"
